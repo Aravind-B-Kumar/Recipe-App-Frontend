a5acc1818f125b19fd6f882fd438e3389a7c418d
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-09 00:40:21 +0530

    View all recipe completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <AddRecipe/>
       <SearchRecipe/>
+      <ViewAll/>
     </div>
   );
 }
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,163 @@
+import React from 'react'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <p class="fs-2">All Recipies</p>
+
+                        <div className="row g-3">
+
+
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipe%20Ramp%20Up%2F2022-02-Air-Fryer-Roasted-Potatoes%2FIMG_6850" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://joyfoodsunshine.com/wp-content/uploads/2022/07/best-omelette-recipe-1.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/meatball-lasagne-e64e936.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2023/08/chole-recipe.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2022/10/vada-pav-recipe-500x500.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://www.seriouseats.com/thmb/HCnqq0O2npAXUoH7_1FdgtZQl0M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pan-roasted-chicken-vegetables-dijon-jus-recipe-hero-02_1-c3e427250efe40259dc8fab9ff66ea74.JPG" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                                <div class="card">
+                                    <img src="https://asset.slimmingworld.co.uk/content/media/19342/sausage-kedgeree-food-range_sw_recipe.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+
+
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://images.immediate.co.uk/production/volatile/sites/30/2023/01/Ginger-chicken-udon-noodles-82fd935.jpg?quality=90&resize=440,400" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+                            </div>
+
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://c.ndtvimg.com/2022-03/j5jbs6g_khichdi_625x300_25_March_22.jpg?im=FeatureCrop,algorithm=dnn,width=384,height=384" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+                            </div>
+
+                            <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.seriouseats.com/thmb/YTLDXO5StrBo2ZIyvFFkOfXMYgc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2020__02__20200218-butter-chicken-vicky-wasik-36-0733fcb29f354c4f946b59bc9e719de7.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="#" class="btn btn-primary">Watch Now</a>
+                                        </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
